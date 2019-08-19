e1783bdb6bea8fa4d258c08edd9435a2e5294d99
ChrisnNg <cristopherng@hotmail.com>
2019-08-19 22:23:33 +0000

    head.js function completed and added

---
+++ head.js
@@ -0,0 +1,14 @@
+const head = function(array) {
+  return array[0];
+};
+
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
+  } else (console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`));
+};
+// TEST CODE
+assertEqual(head([5,6,7]), 5);
+assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+assertEqual(head([]), "Hello");
