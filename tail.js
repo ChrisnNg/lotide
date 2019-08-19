f0655b6ac3d57970cd5be60dc1961dbc64fa50d8
ChrisnNg <cristopherng@hotmail.com>
2019-08-19 22:53:50 +0000

    tail.js added

---
+++ tail.js
@@ -0,0 +1,20 @@
+
+const tail = function(array) {
+  let newarray = [];
+  if (array.length > 1) {
+    newarray.splice(0, 0, array[array.length - 1]);
+  }
+  return newarray;
+};
+
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
+  } else (console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`));
+};
+// Test Case: Check the original array
+const words = ["Yo Yo", "Lighthouse", "Labs"];
+tail(words); // no need to capture the return value since we are not checking it
+assertEqual(words.length, 3); // original array should still have 3 elements!
+console.log(tail(words)); // expected Labs
