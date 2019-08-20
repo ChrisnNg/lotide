ccfcaf562d47286934a850dfaa80f7aadfcf7d00
ChrisnNg <cristopherng@hotmail.com>
2019-08-20 23:29:37 +0000

    added eqArrays.js

---
+++ eqArrays.js
@@ -0,0 +1,26 @@
+/*
+Implement a function eqArrays which takes in two arrays and returns true or false, based on a perfect match.
+
+eqArrays([1, 2, 3], [1, 2, 3]) // => true
+eqArrays([1, 2, 3], [3, 2, 1]) // => false
+
+eqArrays(["1", "2", "3"], ["1", "2", "3"]) // => true
+eqArrays(["1", "2", "3"], ["1", "2", 3]) // => false
+*/
+
+const eqArrays = function(arrayOne, arrayTwo) {
+  for(let i = 0; i < arrayOne.length; i++ ) {
+    if (arrayOne[i] !== arrayTwo[i]) {
+      return false;
+    } 
+  } return true;
+};
+
+// FUNCTION IMPLEMENTATION
+const assertEqual = function(actual, expected) {
+  if (actual === expected) {
+    console.log(`✅✅✅ Assertion passed: ${actual} === ${expected}`);
+  } else (console.log(`🛑🛑🛑 Assertion failed: ${actual} !== ${expected}`));
+};
+// TEST CODE
+assertEqual(eqArrays([1, 2, 3], [1, 2, 3]), true); // => should PASS
