4bd078a3eab12b419efd7fe3ef06f419041448f8
ChrisnNg <cristopherng@hotmail.com>
2019-08-26 20:07:25 +0000

    package.json updated to run \'npm test\' script and headTest.js is now dependent on mocha and chai

--- test/headTest.js
+++ test/headTest.js
@@ -1,6 +1,28 @@
-const head = require("../head");
-const assertEqual = require('../assertEqual');
-// TEST CODE
-assertEqual(head([5,6,7]), 5);
-assertEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
-assertEqual(head([]), "Hello");
+const assert = require('chai').assert;
+const head   = require('../head');
+
+describe("#head", () => {
+  it("returns 1 for [1, 2, 3]", () => {
+    assert.strictEqual(head([1, 2, 3]), 1);
+  });
+
+  it("returns '5' for ['5']", () => {
+    assert.strictEqual(head(['5']), '5');
+  });
+
+  it("returns [5] for [5, 6, 7]", () => {
+    assert.strictEqual(head([5]), 5);
+  });
+
+  it("returns Hello for [\"Hello\", \"Lighthouse\", \"Labs\"]", () => {
+    assert.strictEqual(head(["Hello", "Lighthouse", "Labs"]), "Hello");
+  });
+
+  it("returns undefined for []", () => {
+    assert.strictEqual(head([]), undefined);
+  });
+});
+
+
+
+
