04c2480c9668596098d6a89014b4b09e7ef9e73f
ChrisnNg <cristopherng@hotmail.com>
2019-08-26 20:30:16 +0000

    tailTest.js updated to use mocha and chai

--- test/tailTest.js
+++ test/tailTest.js
@@ -1,11 +1,20 @@
 const tail = require("../tail");
-const assertEqual = require("../assertEqual.js");
-// Test Case 1: Check the returned array elements
-const result = tail(["Hello", "Lighthouse", "Labs"]);
-assertEqual(result.length, 2); // ensure we get back two elements
-assertEqual(result[0], "Lighthouse"); // ensure first element is "Lighthouse"
-assertEqual(result[1], "Labs"); // ensure second element is "Labs"
-// Test Case: Check the original array
-const words = ["Yo Yo", "Lighthouse", "Labs"];
-tail(words); // no need to capture the return value since we are not checking it
-assertEqual(words.length, 3); // original array should still have 3 elements!
+const assert = require('chai').assert;
+
+describe("#tail", () => {
+  it("returns array.length - 1", () => {
+    let array = ["Hello", "Lighthouse", "Labs"];
+    assert.strictEqual(tail(array).length, 2);
+  });
+
+  it("returns [\"Lighthouse\", \"Labs\"] for [\"Hello\", \"Lighthouse\", \"Labs\"]", () => {
+    assert.deepEqual(tail(["Hello", "Lighthouse", "Labs"]), ["Lighthouse", "Labs"]);
+  });
+
+  it("original input should contain the same elements", () => {
+    let array = ["Hello", "Lighthouse", "Labs"];
+    tail(array);
+    assert.strictEqual(array.length, 3);
+  });
+
+});
